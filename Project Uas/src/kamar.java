48db5bde1f47cd7898066b2c29492b50030ccbfd
WilbertWisely <106398772+WilbertWisely@users.noreply.github.com>
2022-07-14 10:15:51 +0700

    Update kamar.java

--- Project Uas/src/kamar.java
+++ Project Uas/src/kamar.java
@@ -4,38 +4,14 @@ public abstract class kamar {
     String namaKamar;
     int harga,kapasitas;
 
-    public String getKode() {
-        return this.kode;
-    }
-
-    public void setKode(String kode) {
-        this.kode = kode;
-    }
-
-    public String getNamaKamar() {
-        return this.namaKamar;
-    }
-
-    public void setNamaKamar(String namaKamar) {
-        this.namaKamar = namaKamar;
-    }
-
-    public int getHarga() {
-        return this.harga;
-    }
-
-    public void setHarga(int harga) {
-        this.harga = harga;
-    }
-
-    public int getKapasitas() {
-        return this.kapasitas;
-    }
-
-    public void setKapasitas(int kapasitas) {
-        this.kapasitas = kapasitas;
-    }
-    
+    abstract public String getKode();
+    abstract public void setKode(String kode);
+    abstract  public String getNamaKamar();
+    abstract public void setNamaKamar(String namaKamar);
+    abstract public int getHarga() ;
+    abstract public void setHarga(int harga) ;
+    abstract public int getKapasitas() ;
+    abstract public void setKapasitas(int kapasitas) ;
    
    
 }
