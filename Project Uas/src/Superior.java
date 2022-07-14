6a3cfbc2e0145b6d6a57e32b9164f2b3ca8a7fa8
WilbertWisely <106398772+WilbertWisely@users.noreply.github.com>
2022-07-14 10:16:35 +0700

    Update Superior.java

--- Project Uas/src/Superior.java
+++ Project Uas/src/Superior.java
@@ -6,7 +6,40 @@ public class Superior extends kamar {
         this.harga = 600000;
         this.kapasitas=4;
     }
+    public String getKode() {
+        return this.kode;
+    }
 
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
+    public String getNamaKamar() {
+        return this.namaKamar;
+    }
+
+    public void setNamaKamar(String namaKamar) {
+        this.namaKamar = namaKamar;
+    }
+
+    public int getHarga() {
+        return this.harga;
+    }
+
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+
+    public int getKapasitas() {
+        return this.kapasitas;
+    }
+
+    public void setKapasitas(int kapasitas) {
+        this.kapasitas = kapasitas;
+    }
+    
+   
+   
     public Superior() {
     }
     public void tampilsuperior() 
